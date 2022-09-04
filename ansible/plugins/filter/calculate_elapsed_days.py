6d1e623091386e024172df6a2a7f6cbc433606e5
gamename <tennis.n.smith@gmail.com>
2022-09-04 11:58:33 -0500

    fixed formatting error

--- ansible/plugins/filter/calculate_elapsed_days.py
+++ ansible/plugins/filter/calculate_elapsed_days.py
@@ -8,13 +8,12 @@ import datetime
 def calculate_elapsed_days(date_stamp):
     """
     Calculates the number of days since the given timestamp.
-    :param date_stamp:
-    :return:
+    :param date_stamp: Date stamp in ISO 8601 format
+    :return: Number of days since the given timestamp
     """
-    print("date_stamp:", date_stamp)
-    date_format = "%Y-%m-%d"
+    # print("date_stamp:", date_stamp)
     today = datetime.date.today()
-    sample_date = datetime.datetime.strptime(str(date_stamp).strip(), date_format)
+    sample_date = datetime.date.fromisoformat(date_stamp)
     delta = sample_date - today
     return delta.days
 
@@ -28,3 +27,16 @@ class FilterModule(object):
         return {
             'calculate_elapsed_days': calculate_elapsed_days
         }
+
+
+def test_calculate_elapsed_days():
+    """
+    Tests the calculate_elapsed_days function
+    :return: Nothing
+    """
+    assert( calculate_elapsed_days(datetime.date.today().isoformat()) == 0)
+
+
+if __name__ == "__main__":
+    test_calculate_elapsed_days()
+
