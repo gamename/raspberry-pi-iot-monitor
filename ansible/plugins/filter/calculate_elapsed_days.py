a4e2720cc0d194f190b0bae0a7e5547ebdea7c7d
gamename <tennis.n.smith@gmail.com>
2022-09-04 11:06:55 -0500

    add filter to calc elapsed days

---
+++ ansible/plugins/filter/calculate_elapsed_days.py
@@ -0,0 +1,23 @@
+"""
+This is to convert a timestamp in the form of yyyy-mm-dd to the number of days since that timestamp.
+"""
+
+from datetime import datetime
+
+def calculate_elapsed_days(date_stamp):
+    date_format = "%Y-%m-%d"
+    today = datetime.date.today()
+    sample_date = datetime.strptime(date_stamp, date_format)
+    delta = sample_date - today
+    return delta.days
+
+
+class FilterModule(object):
+    def filters(self):
+        """
+        Associate the filter with a specific function
+        :return: the filter-to-function association
+        """
+        return {
+            'calculate_elapsed_days': calculate_elapsed_days
+        }
