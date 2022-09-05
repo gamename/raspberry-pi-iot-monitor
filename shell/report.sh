8daa44fdce90ccd6974d6732c676c3cd98c05cfd
gamename <tennis.n.smith@gmail.com>
2022-09-05 07:24:21 -0500

    consolidate regex

--- shell/report.sh
+++ shell/report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "${HOME}/raspberry-pi-iot-monitor/ansible"
-ansible-playbook gather-telemetry.yaml | grep -E "RPIERROR:.*$" -E 'fatal:.[^=]*' -o |
+ansible-playbook gather-telemetry.yaml | grep -E "RPIERROR:.*$|fatal:.[^=]*" -o |
 while read line; do
   python3 "${HOME}/raspberry-pi-iot-monitor/iot/notify.py" \
   --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
