1bc910c032f03e756aee50e74372d44f4b79709e
gamename <tennis.n.smith@gmail.com>
2022-09-07 18:12:48 -0500

    send null report if nothing found

--- shell/report.sh
+++ shell/report.sh
@@ -6,14 +6,26 @@
 #
 
 cd "${HOME}/raspberry-pi-iot-monitor/ansible"
-ansible-playbook gather-telemetry.yaml | grep --only-matching --extended-regexp "RPIERROR:.*$|fatal:.[^=]*"  |
-while read line; do
+gather_out=$(ansible-playbook gather-telemetry.yaml |
+             grep --only-matching --extended-regexp "RPIERROR:.*$|fatal:.[^=]*" )
+
+if [ -z "${gather_out}" ]; then
   python3 "${HOME}/raspberry-pi-iot-monitor/iot/notify.py" \
-  --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
-  --rootCA "${HOME}/root-CA.crt" \
-  --cert "${HOME}/pi-monitor.cert.pem" \
-  --key "${HOME}/pi-monitor.private.key" \
-  --topic "tns/bot/pi-monitor" \
-  --message "MSG003: ${line}"
-done
+    --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
+    --rootCA "${HOME}/root-CA.crt" \
+    --cert "${HOME}/pi-monitor.cert.pem" \
+    --key "${HOME}/pi-monitor.private.key" \
+    --topic "tns/bot/pi-monitor" \
+    --message "MSG003: Fivers"
+else
+  echo "${gather_out}" | while read line; do
+    python3 "${HOME}/raspberry-pi-iot-monitor/iot/notify.py" \
+      --endpoint a3u37c52vq0b6j-ats.iot.us-east-1.amazonaws.com \
+      --rootCA "${HOME}/root-CA.crt" \
+      --cert "${HOME}/pi-monitor.cert.pem" \
+      --key "${HOME}/pi-monitor.private.key" \
+      --topic "tns/bot/pi-monitor" \
+      --message "MSG003: ${line}"
+  done
+fi
 
